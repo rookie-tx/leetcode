010fe8816338200dabfc05cf535ef87e68888bd0
tanxi <yongtanxi@gmail.com>
2021-01-05 17:51:10 +0800

    2020.01.05新增

---
+++ src/No0005.java
@@ -0,0 +1,48 @@
+/**
+ * 5. 最长回文子串
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ *
+ * 示例 1：
+ *
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba" 也是一个有效答案。
+ * 示例 2：
+ *
+ * 输入: "cbbd"
+ * 输出: "bb"
+ *
+ * @author rookie-tx
+ * @version 1.0.0 2021/1/5
+ */
+public class No0005 {
+
+    public String longestPalindrome(String s) {
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > (end - start)) {
+                end = len / 2 + i;
+                start = i - (len - 1) / 2;
+            }
+        }
+
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+
+        return right - left - 1;
+    }
+
+    public static void main(String[] args) {
+        No0005 no0005 = new No0005();
+        System.out.println(no0005.longestPalindrome("bbbb"));
+    }
+}
---
+++ src/No0007.java
@@ -0,0 +1,44 @@
+/**
+ * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+ *
+ * 示例 1:
+ *
+ * 输入: 123
+ * 输出: 321
+ *  示例 2:
+ *
+ * 输入: -123
+ * 输出: -321
+ * 示例 3:
+ *
+ * 输入: 120
+ * 输出: 21
+ * 注意:
+ *
+ * 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reverse-integer
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author rookie-tx
+ * @version 1.0.0 2021/1/5
+ */
+public class No0007 {
+
+    public int reverse(int x) {
+        long reversedNum = 0;
+        while (x != 0) {
+            reversedNum = reversedNum * 10 + x % 10;
+            x /= 10;
+        }
+
+        return reversedNum > Integer.MAX_VALUE || reversedNum < Integer.MIN_VALUE ? 0 : (int) reversedNum;
+    }
+
+    public static void main(String[] args) {
+        No0007 no0007 = new No0007();
+        System.out.println(no0007.reverse(123));
+        System.out.println(no0007.reverse(-123));
+    }
+}
